dded97a99b21f095f6ab43b93b71a65d39ac02ce
mrzalais <lejnieksedijs@gmail.com>
2020-12-02 15:38:51 +0200

    ep2 / a couple tests for ProjectController

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+        Project::create(request(['title', 'description']));
+
+        return redirect('/projects');
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function testUserCanCreateAProject()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+}
