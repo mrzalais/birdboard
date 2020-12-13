0a7a73938cc25f0f170cefc7f2831cf5ae772727
mrzalais <lejnieksedijs@gmail.com>
2020-12-13 15:42:19 +0200

    ep 10 / tailwindcss 2.0 added and styling improved

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,27 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-<h1>Birdboard</h1>
+@extends('layouts.app')
 
-<ul>
-    @forelse($projects as $project)
-        <li>
-            <a href="{{ $project->path() }}">{{ $project->title }}</a>
-        </li>
-    @empty
-        <li>No projects yet.</li>
-    @endforelse
-</ul>
-</body>
-</html>
+@section('content')
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <h2 class="text-grey text-sm font-normal">My projects</h2>
+            <a href="/projects/create" class="btn">Create New Project</a>
+        </div>
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
+        @forelse($projects as $project)
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-light-blue pl-4 mb-4">
+                        <a href="{{ $project->path() }}" class="text-black">{{ $project->title }}</a>
+                    </h3>
+
+                    <div class=""
+                         style="opacity: 0.5">{{ Illuminate\Support\Str::limit($project->description, 50) }}</div>
+                </div>
+            </div>
+        @empty
+            <div>No projects yet.</div>
+        @endforelse
+    </main>
+@endsection
