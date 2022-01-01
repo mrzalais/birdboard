1cf4d6c685306850825b978727ba41b81de0b6b4
mrzalais <lejnieksedijs@gmail.com>
2022-01-01 02:08:09 +0200

    Episode 2 - First Test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'App\Http\Controllers\ProjectsController@index');
+
+Route::post('/projects', 'App\Http\Controllers\ProjectsController@store');
