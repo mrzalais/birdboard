0d478c06cdf3fb00e2f10721a3b42ba5dec79292
mrzalais <lejnieksedijs@gmail.com>
2022-01-05 18:42:23 +0200

    Episode 33 - Inviting Users as a Feature Test

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('/projects/{project}/tasks', 'App\Http\Controllers\ProjectTasksController@store');
     Route::patch('/projects/{project}/tasks/{task}', 'App\Http\Controllers\ProjectTasksController@update');
 
+    Route::post('/projects/{project}/invitations', 'App\Http\Controllers\ProjectInvitationsController@store');
+
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -5,7 +5,9 @@ namespace Tests\Feature;
 use App\Http\Controllers\ProjectTasksController;
 use App\Models\User;
 use Facades\Tests\Setup\ProjectFactory;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class InvitationsTest extends TestCase
@@ -13,7 +15,47 @@ class InvitationsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function a_user_can_be_invited_to_a_project(): void
+    public function non_owners_cannot_invite_users(): void
+    {
+        $user = User::factory()->create();
+
+        /** @var Authenticatable $user */
+        $this->actingAs($user)
+            ->post(ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(Response::HTTP_FORBIDDEN);
+    }
+
+    /** @test */
+    public function a_project_owner_can_invite_a_user(): void
+    {
+        $project = ProjectFactory::create();
+
+        $userToInvite = User::factory()->create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => $userToInvite->email,
+            ])
+            ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    public function the_email_address_must_be_associated_with_a_valid_birdboard_account(): void
+    {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)->post($project->path() . '/invitations', [
+            'email' => 'notauser@example.com',
+        ])
+            ->assertSessionHasErrors([
+                'email' => 'The user you are inviting must have a Birdboard account!'
+            ]);
+    }
+
+    /** @test */
+    public function invited_users_can_update_project_details(): void
     {
         $this->withoutExceptionHandling();
 
